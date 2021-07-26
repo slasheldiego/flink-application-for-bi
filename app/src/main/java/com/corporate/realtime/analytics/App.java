ad446fb3a94725fe7b26d6c6a448bf43c72d8eb2
Diego Armando Benavides Vidal <slasheldiego@gmail.com>
2021-07-25 22:46:20 -0500

    update avro schema

--- app/src/main/java/com/corporate/realtime/analytics/App.java
+++ app/src/main/java/com/corporate/realtime/analytics/App.java
@@ -14,6 +14,7 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.core.fs.Path;
 
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.formats.parquet.avro.ParquetAvroWriters;
